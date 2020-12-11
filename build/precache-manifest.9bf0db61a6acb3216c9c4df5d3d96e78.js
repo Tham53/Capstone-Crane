44d136a506e9722f784b89cedc3ca40b7e30a48c
Tham53 <texas01988@gmail.com>
2020-12-11 15:52:40 -0600

    new

--- build/precache-manifest.9bf0db61a6acb3216c9c4df5d3d96e78.js
+++ build/precache-manifest.9bf0db61a6acb3216c9c4df5d3d96e78.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = [
   {
-    "revision": "b3706b6ee82ddb6693c8",
-    "url": "/Tham53/Capstone-Crane/static/css/main.9a9f2543.chunk.css"
+    "revision": "fa69590a580824481f2c",
+    "url": "/Tham53/Capstone-Crane/static/css/main.8cbc5088.chunk.css"
   },
   {
-    "revision": "b3706b6ee82ddb6693c8",
-    "url": "/Tham53/Capstone-Crane/static/js/main.b3706b6e.chunk.js"
+    "revision": "fa69590a580824481f2c",
+    "url": "/Tham53/Capstone-Crane/static/js/main.fa69590a.chunk.js"
   },
   {
     "revision": "e4f61ba82b2e53833bc5",
@@ -20,7 +20,7 @@ self.__precacheManifest = [
     "url": "/Tham53/Capstone-Crane/static/media/PressStart2P.74496d90.ttf"
   },
   {
-    "revision": "fcd0a9deb81afbbfec70490d9265a699",
+    "revision": "9eddebcda0f0f6e0d60a3f3371b9cbc3",
     "url": "/Tham53/Capstone-Crane/index.html"
   }
 ];
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/Tham53/Capstone-Crane/precache-manifest.2ca26fcf8697e9bf104dda87342817ed.js"
+  "/Tham53/Capstone-Crane/precache-manifest.9bf0db61a6acb3216c9c4df5d3d96e78.js"
 );
 
 workbox.clientsClaim();
