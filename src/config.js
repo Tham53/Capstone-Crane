2219afcadd165f14c7b349b35bada03cc20d9ee7
Tham53 <texas01988@gmail.com>
2020-12-11 14:29:20 -0600

    new

---
+++ src/config.js
@@ -0,0 +1,8 @@
+// Configuration for TMDB
+// Read more about the API here: https://developers.themoviedb.org/
+
+export const auth0Config = {
+  domain: 'dev-a4i9jtmm.us.auth0.com',
+  client_id: 'ifhmc6x1Zgcyr9x7TFOPqrIXMSpypGrv',
+  redirect_uri: 'http://localhost:3000',
+};
